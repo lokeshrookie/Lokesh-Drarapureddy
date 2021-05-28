e598a0f429975dc83b53e96258d430b818b061e3
Lokesh <rocklinglokesh@gmail.com>
2021-05-28 15:43:49 +0530

    fixed two tasks

---
+++ gitfile1.java
@@ -0,0 +1,8 @@
+hi this is lokesh again
+
+
+
+-F
+[A[A[A[A[A[A[B[C[C[C[C[C[C[C[C[C[C[git status
+[D[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cgit checkout master
+lokesh
